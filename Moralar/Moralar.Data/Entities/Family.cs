56fd44697ec596636f8e4795da5ccb0a8e5b9e49
Sérgio Rodrigues Duarte <sergio.rodrigues@megaleios.com>
2021-01-26 11:48:52 -0300

    atualização

---
+++ Moralar/Moralar.Data/Entities/Family.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Moralar.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UtilityFramework.Infra.Core.MongoDb.Data.Modelos;
+
+namespace Moralar.Data.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class Family : ModelBase
+    {
+        /// <summary>
+        /// Dados do Titular
+        /// </summary>
+        public string HolderNumber { get; set; }
+        public string HolderName{get; set;}
+        public string HolderCpf { get; set; }
+        public long HolderBirthday { get; set; }
+        public string HolderGenre { get; set; }
+        public string HolderEmail { get; set; }
+        public string HolderPhone { get; set; }
+        [BsonRepresentation(BsonType.Int32)]
+        public TypeScholarity HolderScholarity { get; set; }
+
+
+        /// <summary>
+        /// Dados do conjuge
+        /// </summary>
+        public string SpouseNumber { get; set; }
+        public string SpouseName { get; set; }
+        public string SpouseCpf { get; set; }
+        public long   SpouseBirthday { get; set; }
+        public string SpouseGenre { get; set; }
+        public string SpouseEmail { get; set; }
+        public string SpousePhone { get; set; }
+        public string SpouseRelationship { get; set; }
+        [BsonRepresentation(BsonType.Int32)]
+        public TypeScholarity SpouseScholarity { get; set; }
+
+
+        /// <summary>
+        /// Dados do membro da Família
+        /// </summary>
+        public string FamilyMemberNumber { get; set; }
+        public string FamilyMemberName { get; set; }
+        public string FamilyMemberCpf { get; set; }
+        public long   FamilyMemberBirthday { get; set; }
+        public string FamilyMemberGenre { get; set; }
+        public string FamilyMemberEmail { get; set; }
+        public string FamilyMemberPhone { get; set; }
+        public string FamilyMemberRelationship { get; set; }
+        [BsonRepresentation(BsonType.Int32)]
+        public TypeScholarity FamilyMemberScholarity { get; set; }
+        [BsonRepresentation(BsonType.Int32)]
+        public TypeKingShip FamilyKinShip { get; set; }
+
+        public override string CollectionName => nameof(Family);
+
+    }
+}
--- Moralar/Moralar.Data/Enum/Enums.cs
+++ Moralar/Moralar.Data/Enum/Enums.cs
@@ -69,7 +69,71 @@ namespace Moralar.Data.Enum
         [EnumMember(Value = "Usuário administrador")]
         [Description("Usuário administrador")]
         UserAdministrator = 1
-
-
+    }
+    public enum TypeScholarity
+    {
+        [EnumMember(Value = "Não possui")]
+        [Description("Não possui")]
+        NaoPossui = 0,
+        [EnumMember(Value = "Fundamental (incompleto)")]
+        [Description("Fundamental (incompleto)")]
+        FundamentalIncompleto = 1,
+        [EnumMember(Value = "Fundamental (completo)")]
+        [Description("Fundamental (completo)")]
+        FundamentalCompleto = 2,
+        [EnumMember(Value = "Médio (incompleto)")]
+        [Description("Médio (incompleto)")]
+        MedioIncompleto = 3,
+        [EnumMember(Value = "Médio (completo)")]
+        [Description("Médio (completo)")]
+        MedioCompleto = 4,
+        [EnumMember(Value = "Superior (incompleto)")]
+        [Description("Superior (incompleto)")]
+        SuperiorIncompleto = 5,
+        [EnumMember(Value = "Superior (completo)")]
+        [Description("Superior (completo)")]
+        SuperiorCompleto = 6,
+        [EnumMember(Value = " Pós-graduação (incompleto)")]
+        [Description(" Pós-graduação (incompleto)")]
+        PosGraduaçãoIncompleto = 7,
+        [EnumMember(Value = "Pós-graduação (completo")]
+        [Description("Pós-graduação (completo")]
+        PosGraduaçãoCompleto = 8
+    }
+    public enum TypeKingShip
+    {
+        [EnumMember(Value = "Filha")]
+        [Description("Filha")]
+        Filha = 0,
+        [EnumMember(Value = "Filho")]
+        [Description("Filho")]
+        Filho = 1,
+        [EnumMember(Value = "Mãe")]
+        [Description("Mãe")]
+        Mae = 2,
+        [EnumMember(Value = "Pai")]
+        [Description("Pai")]
+        Pai = 3,
+        [EnumMember(Value = "Avó")]
+        [Description("Avó")]
+        AvoMulher = 4,
+        [EnumMember(Value = "Avô")]
+        [Description("Avô")]
+        AvoHomem = 5,
+        [EnumMember(Value = "Enteada")]
+        [Description("Enteada")]
+        Enteada = 6,
+        [EnumMember(Value = " Enteado")]
+        [Description("Enteado")]
+        Enteado = 7,
+        [EnumMember(Value = "Tia")]
+        [Description("Tia")]
+        Tia = 8,
+        [EnumMember(Value = "Tio")]
+        [Description("Tio")]
+        Tio = 9,
+        [EnumMember(Value = "Outro")]
+        [Description("Outro")]
+        Outro = 10
     }
 }
